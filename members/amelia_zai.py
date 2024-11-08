1f117fe30c7baa0e496633b777b9bb171ba4a3d1
Amelia Zai <ameliazai@Amelias-MacBook-Pro-2.local>
2024-11-07 21:39:55 -0800

    added amelia zai

---
+++ members/amelia_zai.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Define your information
+name = "Amelia Zai"
+role = "New Member"
+favorite_language = "Python"
+
+# Print the information
+print("--- New Member Information ---")
+print(f"Name: {name}")
+print(f"Role: {role}")
+print(f"Favorite Programming Language: {favorite_language}")
