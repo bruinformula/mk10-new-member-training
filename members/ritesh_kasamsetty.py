ea2dee869132579df3a1fcd79f7eae68b20029d4
ritesh <rkasamse2006@gmail.com>
2024-11-07 20:33:58 -0800

    added Ritesh

---
+++ members/ritesh_kasamsetty.py
@@ -0,0 +1,8 @@
+name = "Ritesh Kasamsetty"
+role = "data vis member"
+favorite_language = "python"
+
+print("--- New Member Information ---")
+print(f"Name: {name}")
+print(f"Role: {role}")
+print(f"Favorite Programming Language: {favorite_language}")
