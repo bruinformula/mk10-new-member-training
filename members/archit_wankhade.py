434e7aa4881fefb09cf8c0d5a8d0365178c2c8cc
Archit Wankhade <awankhade404@student.fuhsd.org>
2024-11-07 20:49:22 -0800

    added data

---
+++ members/archit_wankhade.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Define your information
+name = "Archit Wankhade"
+role = "New Member"
+favorite_language = "CSS"
+
+# Print the information
+print("--- New Member Information ---")
+print(f"Name: {name}")
+print(f"Role: {role}")
+print(f"Favorite Programming Language: {favorite_language}")
