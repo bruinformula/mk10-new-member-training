2144c65f17512997aae30200dd83de6d1547c159
Davis Victor <dvktym@gmail.com>
2024-11-07 20:41:18 -0800

    New member - added my name

---
+++ members/davis_victor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Define your information
+name = "Davis Victor"
+role = "VCU New Member"
+favorite_language = "Python"
+
+# Print the information
+print("--- New Member Information ---")
+print(f"Name: {name}")
+print(f"Role: {role}")
+print(f"Favorite Programming Language: {favorite_language}")
