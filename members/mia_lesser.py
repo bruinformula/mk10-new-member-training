a24f9437716a9f96a3e7a196e8efee818463932c
mia-926 <75146430+mia-926@users.noreply.github.com>
2024-11-07 20:34:17 -0800

    Create mia_lesser.py

---
+++ members/mia_lesser.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Define your information
+name = "Mia Lesser"
+role = "Member"
+favorite_language = "C++"
+
+# Print the information
+print("--- New Member Information ---")
+print(f"Name: {name}")
+print(f"Role: {role}")
+print(f"Favorite Programming Language: {favorite_language}")
