9cbd5243657b37fa06243561ba80882a777f2258
ritesh <rkasamse2006@gmail.com>
2024-11-07 20:36:33 -0800

    commit to my file

---
+++ members/ritesh_kasamsetty.py
@@ -0,0 +1,8 @@
+name = "Ritesh Kasamsetty"
+role = "Data Visualization Frontend"
+favorite_language = "python"
+
+print("--- New Member Information ---")
+print(f"Name: {name}")
+print(f"Role: {role}")
+print(f"Favorite Programming Language: {favorite_language}")
