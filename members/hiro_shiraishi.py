a72f2f11bc5b31c1bcbd97b493dd11edb8686d07
hiroglyph22 <hirotrepreneur@gmail.com>
2024-11-13 21:06:24 -0800

    Create hiro_shiraishi.py

---
+++ members/hiro_shiraishi.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Define your information
+name = "Hiro Shiraishi"
+role = "Software New Member"
+favorite_language = "C++"
+
+# Print the information
+print("--- New Member Information ---")
+print(f"Name: {name}")
+print(f"Role: {role}")
+print(f"Favorite Programming Language: {favorite_language}")
